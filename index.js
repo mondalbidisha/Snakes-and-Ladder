a410d22ae91f5211c405af3cb3ccde5128a89dfa
Mondal Bidisha <mondal.bidisha@talview.com>
2020-09-16 00:09:31 +0530

    snakes and ladder games with only 2 players

--- index.js
+++ index.js
@@ -11,54 +11,69 @@ window.rollDice = () => {
 
 let currentPlayer = players[currentPlayerTurn];
 
-roll = Math.floor(Math.random() * 5 + 1);
-console.log(currentPlayer.name +", You rolled", roll); //keeping track of rolls
-  //incrementing the position after the roll using the dice value
-  if(currentPlayer.position === 0 && roll != 1){ //the first turn has to have 1
-    currentPlayer.position = 0; //otherwise the position stays as it is
-    console.log("Bad luck, you need to roll a 1!") //logs it that I need a one
-  }else{ //else the # of roll is added to the position of the player
-  currentPlayer.position += roll;
-  ladders.forEach(ladder => { //looping through each ladder
-    //if the starting of the ladder is equal to player's position
-    if (ladder.start === currentPlayer.position) {
-      console.log("yay"); //print this on the screen
-      currentPlayer.position = ladder.end; //step to the end of the ladder
+  roll = Math.floor(Math.random() * 5 + 1);
+  console.log(currentPlayer.name +", You rolled", roll); //keeping track of rolls
+    //incrementing the position after the roll using the dice value
+    if(currentPlayer.position === 0 && roll != 1){ //the first turn has to have 1
+      currentPlayer.position = 0; //otherwise the position stays as it is
+      console.log("Bad luck, you need to roll a 1!") //logs it that I need a one
+    } else { //else the # of roll is added to the position of the player
+    currentPlayer.position += roll;
+    ladders.forEach(ladder => { //looping through each ladder
+      //if the starting of the ladder is equal to player's position
+      if (ladder.start === currentPlayer.position) {
+        console.log("yay"); //print this on the screen
+        currentPlayer.position = ladder.end; //step to the end of the ladder
+      }
+    });
+
+    //if the curretPlayer has the last position
+    if (currentPlayer.position > 99) {
+      console.log(currentPlayer.name + " has won!");
+      hasWon = true; //hasWon is true = player wins
     }
-  });
 
-  //if the curretPlayer has the last position
-  if (currentPlayer.position > 99) {
-    console.log(currentPlayer.name + " has won!");
-    hasWon = true; //hasWon is true = player wins
+
+
+    //if it is any other position
+    if (currentPlayer.position === position) {
+      diff = currentPlayer.position - position;
+      currentPlayerPosition = position - diff;
+    }
   }
-
-
-
-//if it is any other position
-  if (currentPlayer.position === position) {
- diff = currentPlayer.position - position;
-    currentPlayerPosition = position - diff;
-  }
-}
   currentPlayerTurn++;
   if (currentPlayerTurn >= players.length) {
     currentPlayerTurn = 0;
   }
 
-drawBoard();
+  drawBoard();
 };
 
 //two players
-players = [{name: "P1",position: 0,color: "gold"},{name: "P2",position: 0,color: "brown"}];
+players = [
+  {
+    name: "P1",
+    position: 0,
+    color: "gold"
+  },
+  {
+    name: "P2",
+    position: 0,
+    color: "brown"
+  }];
 
 let currentPlayerTurn = 0;
 
 board = [];
 let position = 0;
 let darkBox = false;
-ladders = [{start: 2,end: 22},{start: 50,end: 34},{start: 10,end: 44},
-{start: 61,end: 19},{start: 70,end: 83},{start: 78,end: 4}];
+ladders = [
+  { start: 2,  end: 22 },
+  { start: 50, end: 34 },
+  { start: 10, end: 44 },
+  { start: 61, end: 19 },
+  { start: 70, end: 83 },
+  { start: 78, end: 4 }];
 
 for (var y = height; y >= 0; y--) {
   let row = [];
